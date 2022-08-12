ca92fa7b7a809c9835cea18eeee2f966aaadb1db
xiaoming <2604863701@qq.com>
2022-08-12 15:57:50 +0800

    init

---
+++ precache-manifest.981a68cbb8f32d57c890f7fe8a0982e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c28b99ee07857a351d8",
+    "url": "css/app.d600bcbe.css"
+  },
+  {
+    "revision": "28680032b170332d7662e38ecb74e148",
+    "url": "index.html"
+  },
+  {
+    "revision": "5c28b99ee07857a351d8",
+    "url": "js/app.dccbcbed.js"
+  },
+  {
+    "revision": "900d1acf7067e3c6135a",
+    "url": "js/chunk-vendors.50384b13.js"
+  },
+  {
+    "revision": "8743e3c6d3a9deb93a5a625838948152",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
